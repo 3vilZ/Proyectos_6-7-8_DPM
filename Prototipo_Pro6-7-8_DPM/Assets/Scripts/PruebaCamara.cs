f8e1b16b7e43437e954edb505f197540176c6c0b
3vilZ <55482184+3vilZ@users.noreply.github.com>
2020-03-10 21:09:24 +0100

    PruebasController1

---
+++ Prototipo_Pro6-7-8_DPM/Assets/Scripts/PruebaCamara.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaCamara : MonoBehaviour
+{
+    public float fMouseSpeed = 100;
+    public Transform tPlayerBody;
+
+    float fXrot = 0;
+    float fYrot = 0;
+    
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float xMouse = Input.GetAxis("Mouse X") * fMouseSpeed * Time.deltaTime;
+        float yMouse = Input.GetAxis("Mouse Y") * fMouseSpeed * Time.deltaTime;
+
+        fXrot -= yMouse;
+        fXrot = Mathf.Clamp(fXrot, -90, 90);
+
+        transform.localRotation = Quaternion.Euler(fXrot, 0, 0);
+        tPlayerBody.Rotate(Vector3.up * xMouse);
+    }
+}
---
+++ Prototipo_Pro6-7-8_DPM/Assets/Scripts/PruebaPersonaje.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PruebaPersonaje : MonoBehaviour
+{
+    CharacterController cmpCC;
+
+    public float fSpeed = 10;
+    public float fGravity = -9.8f;
+    float fGroundPull = -2;
+
+    public Transform tGroundCheck;
+    public float fGroundRadius;
+    public LayerMask groundMask;
+
+    Vector3 v3Velocity;
+    bool bIsGrounded;
+
+    private void Awake()
+    {
+        cmpCC = GetComponent<CharacterController>();
+    }
+    
+    void Update()
+    {
+        bIsGrounded = Physics.CheckSphere(tGroundCheck.position, fGroundRadius, groundMask);
+
+        if (bIsGrounded && v3Velocity.y < 0)
+        {
+            v3Velocity.y = fGroundPull;
+        }
+
+        float xInput = Input.GetAxis("Horizontal");
+        float zInput = Input.GetAxis("Vertical");
+
+        Vector3 movement = transform.right * xInput + transform.forward * zInput;
+
+        cmpCC.Move(movement * fSpeed * Time.deltaTime);
+
+        v3Velocity.y += fGravity * Time.deltaTime;
+
+        cmpCC.Move(v3Velocity * Time.deltaTime);
+    }
+}
