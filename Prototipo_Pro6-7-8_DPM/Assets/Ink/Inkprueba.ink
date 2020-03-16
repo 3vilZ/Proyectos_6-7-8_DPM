83d50256c5693c9e370fb7255c5527e4a1e1c011
MartinGilOlmedo <62024520+MartinGilOlmedo@users.noreply.github.com>
2020-03-16 14:10:30 +0100

    PruebaInk2

---
+++ Prototipo_Pro6-7-8_DPM/Assets/Ink/Inkprueba.ink
@@ -0,0 +1,8 @@
+Hola Mundo!
+Hola Mundo 2!
+*Pedro
+Terminar 1
+*Daniel
+Terminar2
+*Opcion 3
+Adios!
---
+++ Prototipo_Pro6-7-8_DPM/Assets/InkTestingScript.cs
@@ -0,0 +1,37 @@
+﻿using Ink.Runtime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InkTestingScript : MonoBehaviour
+{
+    public TextAsset inkJSON;
+    private Story story;
+    // Start is called before the first frame update
+    void Start()
+    {
+        story = new Story(inkJSON.text);
+        Debug.Log(loadStoryChunk());
+
+        for (int i = 0; i < story.currentChoices.Count; i++)
+        {
+            Debug.Log(story.currentChoices[i].text);
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    string loadStoryChunk()
+    {
+        string text = "";
+
+        if (story.canContinue)
+        {
+            text = story.ContinueMaximally();
+        }
+            
+        return text;
+    }
+}
