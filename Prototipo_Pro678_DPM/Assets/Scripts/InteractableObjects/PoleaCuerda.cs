1ebd5b8b656c9f0bd1b3ab8d654fb75e1d5cb571
3vilZ <55482184+3vilZ@users.noreply.github.com>
2020-03-21 18:29:00 +0100

    CuerdaPolea

---
+++ Prototipo_Pro678_DPM/Assets/Scripts/InteractableObjects/PoleaCuerda.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoleaCuerda : Interactable
+{
+    public GameObject goInsideItem;
+    public Transform tLocationPoint;
+}
